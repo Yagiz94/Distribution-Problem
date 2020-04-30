20e7331c0e63d56875a54de3087a489bb58dadc2
Yagiz94 <yagizgani94@gmail.com>
2020-04-30 19:59:15 +0300

    Update project.py

--- project.py
+++ project.py
@@ -10,6 +10,7 @@ import random as rand
 
 studentAmount = 75
 list = [0]
+homework_times = [0]
 
 bottom = 100
 top= 300
@@ -28,4 +29,14 @@ for i in range(int(round(N))): # loop size is N * lnN
 
 travel_times = np.array(list)
 travel_times = travel_times[1:travel_times.size]
-print("Travel Times Arr: ", travel_times)
+print("Travel Times Array: ", travel_times)
+
+print("*************** \t ********************")
+print("*************** \t ********************")
+print("*************** \t ********************")
+
+for i in range(studentAmount):
+    
+    homework_times.append(rand.randint(300,500))
+    
+print("Homework Times Array: " , homework_times)
