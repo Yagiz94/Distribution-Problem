14f731e019e4e0bd85ada767b38184345e9739e0
Yagiz94 <yagizgani94@gmail.com>
2020-04-30 19:11:59 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+import numpy as numpy
+import random as random
+
+
+size = 75
+list = []
+
+#for i in range(size):
+i = random.sample(range(100,300), 6)
+list.append(i)
+
+
+print(list)
